75d2c49c7d540de75327f17bfa43247afc1ada3d
beuschc <32576625+beuschc@users.noreply.github.com>
2019-03-21 15:33:52 +0100

    Create siesta.clean.proteins.R

---
+++ R/siesta.clean.proteins.R
@@ -0,0 +1,9 @@
+siesta.clean.proteins <- function(data = data){
+    data <- data %>%
+      filter(is.na(`Only identified by site`)) %>%
+      filter(is.na(`Potential contaminant`)) %>%
+      filter(is.na(Reverse)) %>%
+      filter(Peptides >= 2)
+    
+    return(data)
+  }
