b77bfb24796643bccd0083d41a0c2428f97a5616
beuschc <32576625+beuschc@users.noreply.github.com>
2019-03-29 14:36:34 +0100

    Update siesta_analysis.R

--- R/siesta_analysis.R
+++ R/siesta_analysis.R
@@ -8,29 +8,30 @@ siesta_analysis <- function(results, treatment, vehicle){
     dplyr::select(-c(term, std.error:rSquared)) %>%
     separate(Sample, sep = "_", remove = F, c("Cell_line", "Treatment", "Rep"))
   
-  ggplot(results.t) +
+  g <- ggplot(results.t) +
     geom_density(aes(x = estimate, fill = Sample), alpha = 0.2) +
     facet_wrap(~Treatment, ncol = 2) +
     theme_minimal() +
     theme(legend.position = "bottom")
+ ggsave(g, "Tm_distribution.pdf")
   
-  #ES vs. (E, S, C)
+  #CoFaEnzy vs. (Enzy, CoFa, Cntrl)
   rES <- results.t %>%
     group_by(id) %>%
     do({
       t <- .
       pid <- unique(t$id)
       
-      ES <- t %>% filter(Treatment == "SubEnz")
-      nES <- t %>% filter(Treatment != "SubEnz")
+      ES <- t %>% filter(Treatment == "CoFaEnzy")
+      nES <- t %>% filter(Treatment != "CoFaEnzy")
             
       if(nrow(ES) >= 2 & nrow(nES) >= 2){
         t.test <- try(t.test(ES$estimate, nES$estimate, var.equal = T)$p.value, silent = T)
         
         if(is.numeric(t.test)){
           res <- data.frame("id" = pid,
-                            mean.ES = mean(ES$estimate, na.rm = T),
-                            mean.nES = mean(nES$estimate, na.rm = T),
+                            mean.CoFaEnzy = mean(ES$estimate, na.rm = T),
+                            mean.nCoFaEnzy = mean(nES$estimate, na.rm = T),
                             p.value = t.test)
         }
        else{
@@ -42,9 +43,9 @@ siesta_analysis <- function(results, treatment, vehicle){
        }
       res
     })
-  write_tsv(rES, "ES_vs_nES.tsv")
+  write_tsv(rES, "CoFaEnzy_vs_nCoFaEnzy.tsv")
   
-  #E vs. (S, C)
+  #Enzy vs. (CoFa, Cntrl)
   rE <- results.t %>%
     filter(Treatment != "SubEnz") %>%
     group_by(id) %>%
@@ -52,16 +53,16 @@ siesta_analysis <- function(results, treatment, vehicle){
       t <- .
       pid <- unique(t$id)
       
-      E <- t %>% filter(Treatment == "Enz")
-      nE <- t %>% filter(Treatment != "Enz")
+      E <- t %>% filter(Treatment == "Enzy")
+      nE <- t %>% filter(Treatment != "Enzy")
       
       if(nrow(E) >= 2 & nrow(nE) >= 2){
         t.test <- try(t.test(E$estimate, nE$estimate, var.equal = T)$p.value, silent = T)
         
         if(is.numeric(t.test)){
           res <- data.frame("id" = pid,
-                            mean.E = mean(E$estimate, na.rm = T),
-                            mean.nE = mean(nE$estimate, na.rm = T),
+                            mean.Enzy = mean(E$estimate, na.rm = T),
+                            mean.nEnzy = mean(nE$estimate, na.rm = T),
                             p.value = t.test)
         }
        else{
@@ -73,9 +74,9 @@ siesta_analysis <- function(results, treatment, vehicle){
        }
       res
     })
-  write_tsv(rE, "E_vs_nE.tsv")
+  write_tsv(rE, "Enzy_vs_nEnzy.tsv")
   
-  #S vs. (E, C)
+  #CoFa vs. (Enzy, Cntrl)
   rS <- results.t %>%
     filter(Treatment != "SubEnz") %>%
     group_by(id) %>%
@@ -83,16 +84,16 @@ siesta_analysis <- function(results, treatment, vehicle){
       t <- .
       pid <- unique(t$id)
       
-      S <- t %>% filter(Treatment == "Sub")
-      nS <- t %>% filter(Treatment != "Sub")
+      S <- t %>% filter(Treatment == "CoFa")
+      nS <- t %>% filter(Treatment != "CoFa")
       
       if(nrow(S) >= 2 & nrow(nS) >= 2){
         t.test <- try(t.test(S$estimate, nS$estimate, var.equal = T)$p.value, silent = T)
         
         if(is.numeric(t.test)){
           res <- data.frame("id" = pid,
-                            mean.S = mean(S$estimate, na.rm = T),
-                            mean.nS = mean(nS$estimate, na.rm = T),
+                            mean.CoFa = mean(S$estimate, na.rm = T),
+                            mean.nCoFa = mean(nS$estimate, na.rm = T),
                             p.value = t.test)
         }
        else{
@@ -104,34 +105,35 @@ siesta_analysis <- function(results, treatment, vehicle){
        }
       res
     })
-  write_tsv(rS, "E_vs_nE.tsv")
+  write_tsv(rS, "CoFa_vs_nCoFa.tsv")
   
-  # plot S vs C
+  # plot CoFa vs Cntrl
   p <- results.t %>%
     group_by(id, Treatment) %>%
     summarise(mean.estimate = mean(estimate, na.rm = T))
   
   t <- p %>%
-    filter(Treatment == "Sub" | Treatment == "Cntrl" ) %>%
+    filter(Treatment == "CoFa" | Treatment == "Cntrl" ) %>%
     spread(Treatment, mean.estimate) %>%
     drop_na()
   
-  ggplot(t) +
-    geom_point(aes(x = C, y = ATP), alpha = 0.5) +
+  g <- ggplot(t) +
+    geom_point(aes(x = Cntrl, y = CoFa), alpha = 0.5) +
     theme_minimal()
-  
+  ggsave(g, "CoFa_vs_Cntrl.pdf")
   
   # plot ES-E vs ES-S
   t <- p %>%
     filter(Treatment != "Cntrl") %>%
     spread(Treatment, mean.estimate) %>%
-    mutate("ES-E" = SubEnz - Enz) %>%
-    mutate("ES-S" = SubEnz - Sub) %>%
+    mutate("CoFaEnzy-Enzy" = CoFaEnzy - Enzy) %>%
+    mutate("CoFaEnzy-CoFa" = CoFaEnzy - CoFa) %>%
     drop_na()
   
-  ggplot(t) +
-    geom_point(aes(x = `ES-E`, y = `ES-S`), alpha = 0.5) +
+  g <- ggplot(t) +
+    geom_point(aes(x = `CoFaEnzy-Enzy`, y = `CoFaEnzy-CoFa`), alpha = 0.5) +
     theme_minimal() +
     geom_hline(yintercept = 0) +
     geom_vline(xintercept = 0)
+ ggsave(g, "siesta_plot.pdf")
 }
