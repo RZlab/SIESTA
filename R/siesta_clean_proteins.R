47c5e99ca246ac6afb644fbf3b4f65c98c219831
beuschc <32576625+beuschc@users.noreply.github.com>
2019-03-29 16:43:59 +0100

    Update siesta_clean_proteins.R

--- R/siesta_clean_proteins.R
+++ R/siesta_clean_proteins.R
@@ -13,13 +13,13 @@ siesta_clean_proteins <- function(data = data){
       summarise(nn = n()) %>%
       mutate(prop.nn = nn/sum(nn) * 100)
 
-    g <- ggplot(s_completeness, aes(x = "", y = nn, fill = as.factor(completeness)))+
+    ggplot(s_completeness, aes(x = "", y = nn, fill = as.factor(completeness)))+
         geom_bar(width = 1, stat = "identity") +
         coord_polar("y", start=0) +
         theme_void() +
         geom_text(aes(label = paste0(round(prop.nn, 1), "%")), position = position_stack(vjust = 0.5)) +
         theme(legend.position = "bottom")
-    ggsave(g, "histogram_completeness.pdf")
+    ggsave("histogram_completeness.pdf")
            
     s_replicates <- data %>%
         group_by(Sample) %>%
@@ -27,12 +27,12 @@ siesta_clean_proteins <- function(data = data){
         mutate(n.proteins = n/length(temperatures)) %>%
         mutate(prop.n.proteins = n.proteins/length(unique(data$id)))
 
-     g <- ggplot(s_replicates) +
+     ggplot(s_replicates) +
         geom_bar(aes(x = Sample, y = prop.n.proteins), position = "dodge", stat = "identity") +
         ylim(0, 1) +
         theme_minimal() +
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
-      ggsave(g, "histogram_replicates.pdf")
+      ggsave("histogram_replicates.pdf")
     
     return(data)
   }
