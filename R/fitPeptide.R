f52d469ca83c97871a9f5b992f7d6af87006dd50
beuschc <32576625+beuschc@users.noreply.github.com>
2019-05-13 11:25:18 +0200

    Update fitPeptide.R

--- R/fitPeptide.R
+++ R/fitPeptide.R
@@ -1,4 +1,4 @@
-fitPeptide <- function(pepdata, startPars = c("Pl" = 0, "a" = 550, "b" = 10)){
+fitPeptide <- function(pepdata, startPars = c(Tm = 50, Pl = 0, b = 0.05)){
   pepdata %>%
     group_by(Sample) %>%
     do(
